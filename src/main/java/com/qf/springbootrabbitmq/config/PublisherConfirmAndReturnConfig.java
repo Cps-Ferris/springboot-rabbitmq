4691770153e77756dbbe114c04f8d2836175b3a1
Cai Peishen <peishen.cai@foxmail.com>
2020-10-23 16:45:34 +0800

    千锋教育

---
+++ src/main/java/com/qf/springbootrabbitmq/config/PublisherConfirmAndReturnConfig.java
@@ -0,0 +1,36 @@
+package com.qf.springbootrabbitmq.config;
+
+import org.springframework.amqp.core.Message;
+import org.springframework.amqp.rabbit.connection.CorrelationData;
+import org.springframework.amqp.rabbit.core.RabbitTemplate;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+
+import javax.annotation.PostConstruct;
+
+@Component
+public class PublisherConfirmAndReturnConfig implements RabbitTemplate.ConfirmCallback ,RabbitTemplate.ReturnCallback {
+
+    @Autowired
+    private RabbitTemplate rabbitTemplate;
+
+    @PostConstruct  // init-method
+    public void initMethod(){
+        rabbitTemplate.setConfirmCallback(this);
+        rabbitTemplate.setReturnCallback(this);
+    }
+
+    @Override
+    public void confirm(CorrelationData correlationData, boolean ack, String cause) {
+        if(ack){
+            System.out.println("消息已经送达到Exchange");
+        }else{
+            System.out.println("消息没有送达到Exchange");
+        }
+    }
+
+    @Override
+    public void returnedMessage(Message message, int replyCode, String replyText, String exchange, String routingKey) {
+        System.out.println("消息没有送达到Queue");
+    }
+}
